2621e8c5fc33df89e9145526599052c6aa188199
Ben Ten <b.m.steponavicius@gmail.com>
2023-09-13 15:40:51 +0300

    subtract api calls time spent from sleep time + refactor

--- twitch/_app.py
+++ twitch/_app.py
@@ -1,4 +1,5 @@
 import random
+import utils
 
 from queue import Queue
 from time import sleep
@@ -49,23 +50,37 @@ class TwitchAPP(TwitchAPI):
         self.streams.get_streams()
         return True
 
+    def _sleep(self, secs: float):
+        if secs <= 0:
+            self.cli.print(f'skipping sleep ({secs:.2f})')
+            return
+        self.cli.print(f'sleeping for {secs:.2f} seconds')
+        sleep(secs)
+
+    def _run_api_actions(self):
+        channel_info = self.streams.get_channel_info()
+        self.channel.modify_info(channel_info, True)
+        self.channel.update_description(self.USER_NAME, True)
+        self.shoutout.shoutout(channel_info) or self.raid.random()
+        self.announcements.send()
+        self.polls.create_poll()
+        self.predictions.create_prediction()
+        self.segments.create_stream_schedule_segment()
+        self.clips.create()
+
     def _run_queued_actions(self):
         if self.actions_queue.empty():
             return
         self.actions_queue.get_nowait()()
 
+    def _run_actions(self):
+        self._run_api_actions()
+        self._run_queued_actions()
+
     def api_loop(self):
         while True:
-            channel_info = self.streams.get_channel_info()
-            self.channel.modify_info(channel_info, True)
-            self.channel.update_description(self.USER_NAME, True)
-            self.shoutout.shoutout(channel_info) or self.raid.random()
-            self.announcements.send()
-            self.polls.create_poll()
-            self.predictions.create_prediction()
-            self.segments.create_stream_schedule_segment()
-            self.clips.create()
-            self._run_queued_actions()
-            sleep_time = self.MIN_SLEEP_TIME + (random.random() * self.MAX_SLEEP_DELTA)
-            self.cli.print(f'sleeping for {sleep_time:.2f} seconds')
-            sleep(sleep_time)
+            start_time = utils.get_current_time()
+            self._run_actions()
+            time_spent = utils.get_current_time() - start_time
+            sleep_secs = self.MIN_SLEEP_TIME + (random.random() * self.MAX_SLEEP_DELTA) - time_spent
+            self._sleep(sleep_secs)
